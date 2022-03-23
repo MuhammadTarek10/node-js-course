00aee6c8363d6cd4d64f33eb3664d0ab33bfdaf2
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-03-23 15:38:32 +0200

    updating spaces

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app = express();
 const winston = require('winston');
+const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/logging');
